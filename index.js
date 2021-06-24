ecf4b0e5e2c242395c3a678fe0fbe063fcd97748
Xanthe McElroy <78177169+xanthemce@users.noreply.github.com>
2021-06-24 21:19:34 +0000

    I added a question to allow the user to input something

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //list of players ready to be picked
 players = [' anna', ' emma', ' lily', ' ella', ' elle', ' ava', ' dayna', ' negg', ' eve', ' eva', ' evie', ' bob', ' ricky'];
 
+var confirm = prompt('would you like to randomly distribute these players into two teams: yes or no' + players)
+
 
 
 //sets value for round
@@ -12,14 +14,15 @@ let team1 = []
 //sets team2 as a list
 let team2 = []
 //starts loop
+
+if (confirm == 'yes'){
 do {
 //random pick
 //picks a random number
 random1 = Math.floor(Math.random() * length + 0)
 //prints name associated with that number in the list
 player1 = (players[random1])
-//prints the random player
-console.log(player1)
+
 //adds randomly slected player to team 1
 team1.push(player1)
 
@@ -27,22 +30,21 @@ team1.push(player1)
 players.splice(random1,1)
 //takes 1 off the length of list of players
 length--
-//prints list of players
-console.log(players)
+
+
 
 //random pick
 //picks a random number
 random2 = Math.floor(Math.random() * players.length + 0)
 //prints name associated with that number in the list
 player2 = (players[random2])
-console.log(player2)
+
 //adds selected player to team 2
 team2.push(player2)
 //remove random person from list
 players.splice(random2,1)
 
-//prints players
-console.log(players)
+
 //adding 1 to the value of team
 round++
 //taking 1 off the length of the players list
@@ -55,5 +57,10 @@ while (round < 6)
 
 //printing the list of members in the team
 
-console.log(' this is team1: ' + team1)
-console.log('this is team2: ' + team2)
+console.log(' this is team 1: ' + team1)
+console.log('this is team 2: ' + team2)
+}
+
+else{
+  console.log('okay.')
+}
